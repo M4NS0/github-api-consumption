a59056d46663a830e110bf085887997cb2896469
Bruno C. Manso <mrbrunomanso@gmail.com>
2023-04-19 11:25:25 -0300

    landing-page interface modeling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
